8a54272ca9d11f05315d04af39d00caeec831236
fjwillemsen <fjwillemsen@icloud.com>
2022-08-09 09:41:53 +0200

    Fixed an issue where the update dialog did not point to the latest release

--- Scripts/update.js
+++ Scripts/update.js
@@ -8,53 +8,55 @@ async function fetchAsync(url) {
 
 function semanticVersionCompare(a, b) {
     // from https://gist.github.com/iwill/a83038623ba4fef6abb9efca87ae9ccb
-    if (a.startsWith(b + "-")) return -1
-    if (b.startsWith(a + "-")) return  1
-    return a.localeCompare(b, undefined, { numeric: true, sensitivity: "case", caseFirst: "upper" })
+    if (a.startsWith(b + "-")) return -1;
+    if (b.startsWith(a + "-")) return 1;
+    return a.localeCompare(b, undefined, { numeric: true, sensitivity: "case", caseFirst: "upper" });
 }
 
 async function checkForUpdate(reportAll = false) {
-    tags = await fetchAsync("https://api.github.com/repos/fjwillemsen/NativeOverleaf/tags")
-    if (!(tags.length) || tags.length === undefined) {
-        console.error("Can not retrieve latest version for update checking")
-        return
+    tags = await fetchAsync("https://api.github.com/repos/fjwillemsen/NativeOverleaf/tags");
+    if (!tags.length || tags.length === undefined) {
+        console.error("Can not retrieve latest version for update checking");
+        return;
     }
-    latest_version = tags[0].name.replace('v', '')
-    comparison = semanticVersionCompare(latest_version, appversion)
+    latest_version = tags[0].name.replace("v", "");
+    comparison = semanticVersionCompare(latest_version, appversion);
     if (comparison == 0 && comparison !== "") {
-        console.log("Update check completed, no update available.")
+        console.log("Update check completed, no update available.");
         if (reportAll == true) {
-            alert("You're up to date with the latest version!")
+            alert("You're up to date with the latest version!");
         }
     } else if (comparison == 1) {
         goToUpdate = confirm(`Update available! 
             Current: ${appversion}, latest: ${latest_version}.
-            Go to downloads page?`)
+            Go to downloads page?`);
         if (goToUpdate) {
-            window.open("https://github.com/fjwillemsen/NativeOverleaf/releases/tag/v1.2.0")
+            window.open("https://github.com/fjwillemsen/NativeOverleaf/releases/latest/");
         }
     } else if (comparison == -1) {
-        result = `No update needed, current version (${appversion}) is newer than latest publicly available version (${latest_version}).`
-        console.log(result)
+        result = `No update needed, current version (${appversion}) is newer than latest publicly available version (${latest_version}).`;
+        console.log(result);
         if (reportAll == true) {
-            alert(result)
+            alert(result);
         }
     } else {
-        result = `Invalid semantic version comparison outcome: ${comparison}`
-        console.log(result)
+        result = `Invalid semantic version comparison outcome: ${comparison}`;
+        console.log(result);
         if (reportAll == true) {
-            alert(result)
+            alert(result);
         }
     }
 }
 
 function setAutoUpdateChecking() {
     // check for updates straight away
-    checkForUpdate()
+    checkForUpdate();
     // check for updates every six hours
-    setInterval(checkForUpdate, 6*60*60*1000);
+    setInterval(checkForUpdate, 6 * 60 * 60 * 1000);
     // trigger the check update function when the version label is clicked
-    if (document.querySelector('#versionlabel')) {
-        document.querySelector('#versionlabel').onclick = function(){ checkForUpdate(true) }
+    if (document.querySelector("#versionlabel")) {
+        document.querySelector("#versionlabel").onclick = function () {
+            checkForUpdate(true);
+        };
     }
 }
